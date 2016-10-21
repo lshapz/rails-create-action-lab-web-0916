a67417b5271f30cd662af0d0c5a58e27d01a93f2
lshapz <laurashapiro01@gmail.com>
2016-10-21 19:50:44 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,19 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    #byebug
+    @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
+    @student.save
+    redirect_to student_path(@student)
   end
 
+
+private 
+def student_params
+  params.require(:student).permit(:first_name, :last_name)
+end 
 end
--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -26,7 +26,7 @@ describe 'form page' do
   it 'new form submits content and renders form content' do
     visit new_student_path
 
-    fill_in 'first_name', with: "Margaery"
+    fill_in 'first_name', with: "Margaery" 
     fill_in 'last_name', with: "Tyrell"
 
     click_on "Submit Student"
